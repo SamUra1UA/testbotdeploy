7fa716b263192ed47f0d087d85348f37ecd099af
Yevhenii <146078178+EvheniiOrza@users.noreply.github.com>
2025-05-21 22:56:12 +0300

    Addet new chat

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ API_ID = os.getenv("YOUR_API_ID")
 API_HASH = os.getenv("YOUR_API_HASH")
 SESSION_NAME = "gingers_session"
 
-SOURCE_CHAT_ID = [-1002293398473, -1002270373322, -1002293398473]
+SOURCE_CHAT_ID = [-1002293398473, -1002270373322, -1002293398473, -1002628565313, -1002696474292]
 DESTINATION_CHAT_ID = -1002334681781
 
 THREAD_MAPPING = {
@@ -29,6 +29,8 @@ THREAD_MAPPING = {
     57918:3852,
 }
 FALLBACK_THREAD_ID = 988
+FALLBACK_THREAD_ID_2 = 5054
+FALLBACK_THREAD_ID_TEST = 5057
 
 MESSAGE_MAPPING_FILE = 'message_mapping.json'
 
@@ -61,9 +63,30 @@ async def forward_message(event):
     try:
         destination_thread = None
 
+        media = event.message.media
+        text = event.message.message or ""
+        original_buttons = event.message.buttons or []
+        links = extract_links(text)
+        link_buttons = generate_link_buttons(links)
+        combined_buttons = original_buttons + [[btn] for btn in link_buttons] if link_buttons else original_buttons
+
         # Канал без гілок
         if event.chat_id == -1002270373322:
             destination_thread = FALLBACK_THREAD_ID
+            if event.message.reply_to:
+                source_reply_id = event.message.reply_to.reply_to_msg_id
+                if source_reply_id in MESSAGE_MAPPING:
+                    destination_thread = MESSAGE_MAPPING[source_reply_id]
+
+        elif event.chat_id == -1002628565313:
+            destination_thread = FALLBACK_THREAD_ID_2
+            if event.message.reply_to:
+                source_reply_id = event.message.reply_to.reply_to_msg_id
+                if source_reply_id in MESSAGE_MAPPING:
+                    destination_thread = MESSAGE_MAPPING[source_reply_id]
+
+        elif event.chat_id == -1002696474292:
+            destination_thread = FALLBACK_THREAD_ID_TEST
             if event.message.reply_to:
                 source_reply_id = event.message.reply_to.reply_to_msg_id
                 if source_reply_id in MESSAGE_MAPPING:
@@ -81,13 +104,6 @@ async def forward_message(event):
         if not destination_thread:
             print("⚠️ Повідомлення пропущене: немає відповідної гілки.")
             return
-
-        media = event.message.media
-        text = event.message.message or ""
-        original_buttons = event.message.buttons or []
-        links = extract_links(text)
-        link_buttons = generate_link_buttons(links)
-        combined_buttons = original_buttons + [[btn] for btn in link_buttons] if link_buttons else original_buttons
 
         sent_message = None
 
